b28cac9213eddc7ac1dfe364193314ea6ad6ea4e
frank89722 <frank89722@gmail.com>
2022-01-08 02:37:00 +0800

    use record

--- src/main/java/frankv/jmi/jmdefaultconfig/JMDefualtConfig.java
+++ src/main/java/frankv/jmi/jmdefaultconfig/JMDefualtConfig.java
@@ -3,12 +3,14 @@ package frankv.jmi.jmdefaultconfig;
 import frankv.jmi.JMI;
 
 public class JMDefualtConfig {
-    private static Version existVersion;
+
+
+    private static FileManager.Version existVersion;
 
     public static void tryWriteJMDefaultConfig() {
         existVersion = FileManager.readConfigVersion();
 
-        if (existVersion.version < JMI.CLIENT_CONFIG.getDefaultConfigVersion()) {
+        if (existVersion.version() < JMI.CLIENT_CONFIG.getDefaultConfigVersion()) {
             FileManager.writeJMDefaultConfig(JMI.CLIENT_CONFIG.getDefaultConfigVersion());
         }
     }
--- src/main/java/frankv/jmi/jmdefaultconfig/Version.java
+++
@@ -1,9 +0,0 @@
-package frankv.jmi.jmdefaultconfig;
-
-public class Version {
-    public final int version;
-
-    public Version(int version) {
-        this.version = version;
-    }
-}
