728d4674329d26c6e327fc94126c98408f422465
frank89722 <frank89722@gmail.com>
2022-01-09 19:37:05 +0800

    Revert "use record"
    This reverts commit b28cac92

--- src/main/java/frankv/jmi/jmdefaultconfig/JMDefualtConfig.java
+++ src/main/java/frankv/jmi/jmdefaultconfig/JMDefualtConfig.java
@@ -3,12 +3,12 @@ package frankv.jmi.jmdefaultconfig;
 import frankv.jmi.JMI;
 
 public class JMDefualtConfig {
-    private static FileManager.Version existVersion;
+    private static Version existVersion;
 
     public static void tryWriteJMDefaultConfig() {
         existVersion = FileManager.readConfigVersion();
 
-        if (existVersion.version() < JMI.CLIENT_CONFIG.getDefaultConfigVersion()) {
+        if (existVersion.version < JMI.CLIENT_CONFIG.getDefaultConfigVersion()) {
             FileManager.writeJMDefaultConfig(JMI.CLIENT_CONFIG.getDefaultConfigVersion());
         }
     }
---
+++ src/main/java/frankv/jmi/jmdefaultconfig/Version.java
@@ -0,0 +1,9 @@
+package frankv.jmi.jmdefaultconfig;
+
+public class Version {
+    public final int version;
+
+    public Version(int version) {
+        this.version = version;
+    }
+}
