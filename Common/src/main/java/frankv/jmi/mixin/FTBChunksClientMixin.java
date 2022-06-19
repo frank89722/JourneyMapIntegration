9a600357dcedacbb34ede220328de07a5d0229b2
frank89722 <frank89722@gmail.com>
2022-06-19 22:14:04 +0800

    cleanup mixins

--- Common/src/main/java/frankv/jmi/mixin/FTBChunksClientMixin.java
+++ Common/src/main/java/frankv/jmi/mixin/FTBChunksClientMixin.java
@@ -2,7 +2,7 @@ package frankv.jmi.mixin;
 
 import dev.ftb.mods.ftbchunks.client.ChunkScreen;
 import dev.ftb.mods.ftbchunks.client.FTBChunksClient;
-import frankv.jmi.JMIForge;
+import frankv.jmi.JMI;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -13,7 +13,7 @@ public class FTBChunksClientMixin {
 
     @Inject(method = "openGui", at = @At("HEAD"), cancellable = true, remap = false)
     private static void regionScreenFirst(CallbackInfo ci) {
-        if (!JMIForge.CLIENT_CONFIG.getShowClaimChunkScreen()) return;
+        if (!JMI.clientConfig.getShowClaimChunkScreen()) return;
         (new ChunkScreen()).openGui();
         ci.cancel();
     }
