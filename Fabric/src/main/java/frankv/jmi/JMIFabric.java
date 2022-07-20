aca403b368b7d3c5fe234d4545ace5e380a4db05
frank89722 <mail@frankv.me>
2022-07-20 19:29:04 +0800

    fix crash on server

--- Fabric/src/main/java/frankv/jmi/JMIFabric.java
+++ Fabric/src/main/java/frankv/jmi/JMIFabric.java
@@ -3,13 +3,13 @@ package frankv.jmi;
 import frankv.jmi.config.ClientConfig;
 import frankv.jmi.config.IClientConfig;
 import frankv.jmi.jmdefaultconfig.JMDefaultConfig;
-import net.fabricmc.api.ModInitializer;
+import net.fabricmc.api.ClientModInitializer;
 
-public class JMIFabric implements ModInitializer {
+public class JMIFabric implements ClientModInitializer {
     public static final IClientConfig CLIENT_CONFIG = ClientConfig.loadConfig();
 
     @Override
-    public void onInitialize() {
+    public void onInitializeClient() {
         JMI.init(CLIENT_CONFIG, new JMIFabricEventListener());
         new JMDefaultConfig().tryWriteJMDefaultConfig();
     }
